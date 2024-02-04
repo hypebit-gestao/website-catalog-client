37bb8351181a276549bc5824763d159234b1d7f1
rafaelcf00 <rafacesar0070@gmail.com>
2024-02-03 22:39:03 -0300

    mercadopago

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
 "use client";
 
+import { useMercadoPagoService } from "@/services/mercadopago.service";
+import { Wallet, initMercadoPago } from "@mercadopago/sdk-react";
+import { useState } from "react";
+
 export default function Home() {
+  initMercadoPago("APP_USR-52b7b05a-4c58-432b-9f60-41cccc75369d");
+  const mercadoPagoService = useMercadoPagoService();
+  const [preferenceId, setPreferenceId] = useState();
+
+  const obj = {
+    items: [
+      {
+        title: "Assinatura Mensal",
+        quantity: 1,
+        currency_id: "BRL",
+        unit_price: 40.0,
+      },
+    ],
+    subscription: {
+      transaction_amount: 40.0,
+      frequency: 1,
+      frequency_type: "months",
+    },
+  };
+
+  const createPreference = async () => {
+    await mercadoPagoService
+      .POSTPREFERENCE(obj)
+      .then((res) => setPreferenceId(res.id));
+  };
+
+  console.log("Preference ID: ", preferenceId);
+
   return (
     <main className="flex justify-center items-center min-h-screen w-full bg-green-primary">
       <div className="text-2xl text-white">CADASTRE SUA LOJA CONOSCO</div>
+      <div>
+        <button
+          onClick={() => createPreference()}
+          className="p-4 rounded-md bg-blue-600"
+        >
+          Pagar
+        </button>
+      </div>
+      {preferenceId && (
+        <div id="wallet_container">
+          <Wallet
+            initialization={{ preferenceId: preferenceId as any }}
+            customization={{ texts: { valueProp: "smart_option" } }}
+          />
+        </div>
+      )}
     </main>
   );
 }
--- src/utils/functions/fetch.ts
+++ src/utils/functions/fetch.ts
@@ -2,23 +2,20 @@ export async function fetchWrapper<T = unknown>(
   input: RequestInfo | URL,
   init?: RequestInit | undefined
 ) {
-  const data = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/${input}`,
-    init
-  );
+  const data = await fetch(`${input}`, init);
 
   if (!data.ok) {
     return;
   }
 
-    // Check if the response is empty
-    const text = await data.text();
-    let result;
-    if (text.length) {
-      result = JSON.parse(text);
-    } else {
-      result = {};
-    }
-  
-    return result as T;
+  // Check if the response is empty
+  const text = await data.text();
+  let result;
+  if (text.length) {
+    result = JSON.parse(text);
+  } else {
+    result = {};
+  }
+
+  return result as T;
 }
