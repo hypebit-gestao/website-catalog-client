8495d7756ea3c4d19d95587762aa00f54887df8d
rafaelcf00 <rafacesar0070@gmail.com>
2024-06-01 19:58:34 -0300

    updates

--- src/app/[loja]/page.tsx
+++ src/app/[loja]/page.tsx
@@ -5,7 +5,7 @@ import Container from "@/components/container";
 import ProductCard from "@/components/product-card";
 import { useProductService } from "@/services/product.service";
 import { useParams, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import { Product } from "@/models/product";
 import Loader from "@/components/loader";
@@ -17,6 +17,7 @@ import { Button } from "@/components/ui/button";
 import { useUserService } from "@/services/user.service";
 import { User } from "@/models/user";
 import useStore from "@/utils/hooks/use-store";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Catalog = () => {
   const params = useParams();
@@ -34,6 +35,7 @@ const Catalog = () => {
   const { addItem, items, totalItems } = useCart();
   const search = useSearch();
   const router = useRouter();
+  const featuredScrollRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setLoading(true);
@@ -114,19 +116,37 @@ const Catalog = () => {
     if (filteredProducts?.length) return category;
   });
 
+  const scrollLeft = () => {
+    if (featuredScrollRef.current) {
+      featuredScrollRef.current.scrollBy({
+        left: -350,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (featuredScrollRef.current) {
+      featuredScrollRef.current.scrollBy({
+        left: 350,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className=" min-h-screen ">
+    <div className="min-h-screen">
       {filteredCategories?.length > 0 && (
-        <div className="flex flex-row justify-center w-full h-full items-center  py-5 overflow-auto xl:overflow-hidden">
+        <div className="flex flex-row justify-center w-full h-full items-center py-5 overflow-auto xl:overflow-hidden">
           <Container>
             {loading ? (
               <Loader />
             ) : (
               <div
                 id="categories"
-                className="w-full flex flex-col xl:flex-row items-center "
+                className="w-full flex flex-col xl:flex-row items-center"
               >
-                <div className="w-full flex flex-row items-center xl:justify-normal ">
+                <div className="w-full flex flex-row items-center xl:justify-normal">
                   {filteredCategories?.map(
                     (category: Category, index: number) => (
                       <CategoryItem
@@ -170,41 +190,65 @@ const Catalog = () => {
                 (product) => product.featured && product.active
               ) && (
                 <>
-                  <h1
-                    style={{
-                      color:
-                        store?.store?.background_color !== null
-                          ? `${store?.store?.background_color}`
-                          : "#1e3222",
-                    }}
-                    className="text-green-secondary text-3xl mb-6"
-                  >
-                    Produtos em destaque
-                  </h1>
-                  <div className="w-full  gap-y-16 grid grid-cols grid-cols-2 gap-x-2 lg:gap-x-16 lg:grid-cols-3 xl:grid-cols-4">
-                    {products?.map(
-                      (product, index) =>
-                        product.featured && (
-                          <div key={index} className="">
-                            <ProductCard
+                  <div className="w-full flex justify-between items-center">
+                    <h1
+                      style={{
+                        color:
+                          store?.store?.background_color !== null
+                            ? `${store?.store?.background_color}`
+                            : "#1e3222",
+                      }}
+                      className="text-green-secondary text-3xl mb-6"
+                    >
+                      Produtos em destaque
+                    </h1>
+                    <div className="flex items-center gap-x-5">
+                      <button
+                        onClick={scrollLeft}
+                        className=" p-3  border border-gray-200 rounded-full shadow-md "
+                      >
+                        <FaArrowLeft color="#2c6e49" />
+                      </button>
+                      <button
+                        onClick={scrollRight}
+                        className=" p-3  rounded-full shadow-md "
+                      >
+                        <FaArrowRight color="#2c6e49" />
+                      </button>
+                    </div>
+                  </div>
+                  <div className="relative flex items-center">
+                    <div
+                      className="w-full overflow-x-auto whitespace-nowrap scroll-smooth"
+                      ref={featuredScrollRef}
+                    >
+                      {products?.map(
+                        (product, index) =>
+                          product.featured && (
+                            <div
                               key={index}
-                              onClick={() =>
-                                router.push(`/${loja}/${product.id}`)
-                              }
-                              name={product.name}
-                              price={product.price}
-                              promotionPrice={product.promotion_price}
-                              images={
-                                product.images && product.images.length > 0
-                                  ? product.images
-                                  : [
-                                      "https://www.pallenz.co.nz/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png",
-                                    ]
-                              }
-                            />
-                          </div>
-                        )
-                    )}
+                              className="inline-block w-1/2 lg:w-1/4 pr-6 last:pr-0"
+                            >
+                              <ProductCard
+                                key={index}
+                                onClick={() =>
+                                  router.push(`/${loja}/${product.id}`)
+                                }
+                                name={product.name}
+                                price={product.price}
+                                promotionPrice={product.promotion_price}
+                                images={
+                                  product.images && product.images.length > 0
+                                    ? product.images
+                                    : [
+                                        "https://www.pallenz.co.nz/assets/camaleon_cms/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1a654fc54840aac61a57a60903fef.png",
+                                      ]
+                                }
+                              />
+                            </div>
+                          )
+                      )}
+                    </div>
                   </div>
                 </>
               )}
@@ -266,7 +310,7 @@ const Catalog = () => {
                     <Button
                       onClick={loadMoreProducts}
                       size="xl"
-                      className=" w-[50%] md:w-[30%] text-lg"
+                      className="w-[50%] md:w-[30%] text-lg"
                       type="submit"
                       variant={"default"}
                     >
